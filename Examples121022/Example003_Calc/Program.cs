a0fc250a1292432795e51206de9ba84560cfda41
AnastasiaBuglakova <bugla2005@mail.ru>
2022-10-12 22:15:49 +0300

    random added2

---
+++ Examples121022/Example003_Calc/Program.cs
@@ -0,0 +1,6 @@
+﻿double numberA = new Random ().Next (1, 10); // 1, 2 ... 9
+double numberB = new Random ().Next (1, 10);
+
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA / numberB);
