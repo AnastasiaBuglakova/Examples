91b77a3c59aebf83fc55f0e0ebab87b107c5dd78
AnastasiaBuglakova <bugla2005@mail.ru>
2022-10-13 08:53:35 +0300

    Masha added

---
+++ Examples121022/Example005_superMasha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Введите имя пользователя ");
+string username = Console.ReadLine ();
+
+if (username == маша);
+{
+    Console.WriteLine("Машенька, дорогая, привет!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
